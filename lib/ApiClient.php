168bebcf8d3fdb2841682f8e62831fe117a7568d
Anton Novopashin <antonfromkrsk@gmail.com>
2017-03-01 15:04:39 +0300

    добавлены методы магазинов и легалов

--- lib/ApiClient.php
+++ lib/ApiClient.php
@@ -163,4 +163,144 @@ class ApiClient
 
         return $this->client->makeRequest('settings/calls/operators/' . $id, 'PUT', $params);
     }
+
+    /**
+     * @param string $id
+     * @return Response\ApiResponse
+     */
+    public function getShopList()
+    {
+        return $this->client->makeRequest('shops/', 'GET', array());
+    }
+
+    /**
+     * @param string $title
+     * @param string $url
+     * @return Response\ApiResponse
+     */
+    public function addShop(string $title, string $url)
+    {
+        $params = [];
+        $params['title'] = $title;
+        $params['url'] = $url;
+
+        return $this->client->makeRequest('shops/', 'POST', $params);
+    }
+
+    /**
+     * @param string $id
+     *
+     * /shops/{shopId}
+     *
+     * @return Response\ApiResponse
+     */
+    public function getShop(string $id)
+    {
+        return $this->client->makeRequest('shops/' . $id, 'GET', array());
+    }
+
+    /**
+     * @param string $id
+     * @param string $title
+     * @param string $url
+     * @return Response\ApiResponse
+     */
+    public function updateShop(string $id, string $title, string $url)
+    {
+        $params = [];
+        $params['title'] = $title;
+        $params['url'] = $url;
+
+        return $this->client->makeRequest('shops/' . $id, 'PUT', $params);
+    }
+
+    /**
+     * @param string $id
+     * @return Response\ApiResponse
+     */
+    public function removeShop(string $id)
+    {
+        return $this->client->makeRequest('shops/' . $id, 'DELETE', array());
+    }
+
+    /**
+     * @param string $id
+     * @return Response\ApiResponse
+     */
+    public function getShopLegalList(string $id)
+    {
+        return $this->client->makeRequest('shops/' . $id . '/legals', 'GET', array());
+    }
+
+    /**
+     * @param string $id shop id
+     * @param string $name
+     * @param string $inn length = 10 || 12
+     * @param string $bik
+     * @param string $bank
+     * @param string $corrAccount length = 20
+     * @param string $finAccount length = 20
+     * @param string $address
+     * @return Response\ApiResponse
+     */
+    public function addLegal(string $id, string $name, string $inn, string $bik, string $bank, string $corrAccount, string $finAccount, string $address)
+    {
+        $params = [];
+        $params['name'] = $name;
+        $params['inn'] = $inn;
+        $params['bik'] = $bik;
+        $params['bank'] = $bank;
+        $params['corr_account'] = $corrAccount;
+        $params['fin_account'] = $finAccount;
+        $params['address'] = $address;
+
+        return $this->client->makeRequest('/shops/' . $id . '/legals', 'POST', $params);
+    }
+
+    /**
+     * @param string $id
+     * @param string $legalId
+     * @return Response\ApiResponse
+     */
+    public function getLegal(string $id, string $legalId)
+    {
+        return $this->client->makeRequest('/shops/' . $id . '/legals/' . $legalId, 'GET', array());
+    }
+
+    /**
+     * @param string $id
+     * @param string $legalId
+     * @param string $name
+     * @param string $inn
+     * @param string $bik
+     * @param string $bank
+     * @param string $corrAccount
+     * @param string $finAccount
+     * @param string $address
+     * @return Response\ApiResponse
+     */
+    public function updateLegal(string $id, string $legalId, string $name, string $inn, string $bik, string $bank, string $corrAccount, string $finAccount, string $address)
+    {
+        $params = [];
+        $params['name'] = $name;
+        $params['inn'] = $inn;
+        $params['bik'] = $bik;
+        $params['bank'] = $bank;
+        $params['corr_account'] = $corrAccount;
+        $params['fin_account'] = $finAccount;
+        $params['address'] = $address;
+
+        return $this->client->makeRequest('/shops/' . $id . '/legals/' . $legalId, 'PUT', $params);
+    }
+
+    /**
+     * @param string $id
+     * @param string $legalId
+     * @return Response\ApiResponse
+     */
+    public function removeLegal(string $id, string $legalId)
+    {
+        return $this->client->makeRequest('/shops/' . $id . '/legals/' . $legalId, 'DELETE', array());
+    }
+
 }
