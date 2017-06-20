f519ce35cc17ec091d5610aee608b194a19c80da
Alex Morbo <alex@morbo.ru>
2017-06-20 15:46:50 +0300

    POST, PUT now send json

--- src/PostRadar/Http/Client.php
+++ src/PostRadar/Http/Client.php
@@ -74,6 +74,9 @@ class Client
         }
 
         $curlHandler = curl_init();
+        $headers = [
+            'X-Api-Token: '.$parameters['X-Api-Token'],
+        ];
         curl_setopt($curlHandler, CURLOPT_URL, $url);
         curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curlHandler, CURLOPT_FOLLOWLOCATION, 1);
@@ -82,22 +85,19 @@ class Client
         curl_setopt($curlHandler, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curlHandler, CURLOPT_TIMEOUT, 30);
         curl_setopt($curlHandler, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($curlHandler, CURLOPT_HTTPHEADER, [
-                'X-Api-Token: '.$parameters['X-Api-Token'],
-            ]
-        );
 
         if (self::METHOD_POST === $method) {
             curl_setopt($curlHandler, CURLOPT_POST, true);
-            curl_setopt($curlHandler, CURLOPT_POSTFIELDS, http_build_query($parameters));
+            curl_setopt($curlHandler, CURLOPT_POSTFIELDS, json_encode($parameters));
+            $headers[] = 'Content-Type: application/json';
         }
 
         if (self::METHOD_PUT === $method) {
             curl_setopt($curlHandler, CURLOPT_HEADER, 0);
             curl_setopt($curlHandler, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($curlHandler, CURLOPT_POSTFIELDS, http_build_query($parameters));
+            curl_setopt($curlHandler, CURLOPT_POSTFIELDS, json_encode($parameters));
             curl_setopt($curlHandler, CURLOPT_CUSTOMREQUEST, "PUT");
-
+            $headers[] = 'Content-Type: application/json';
         }
 
         if (self::METHOD_DELETE === $method) {
@@ -105,6 +105,8 @@ class Client
             curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $parameters);
             curl_setopt($curlHandler, CURLOPT_CUSTOMREQUEST, "DELETE");
         }
+
+        curl_setopt($curlHandler, CURLOPT_HTTPHEADER, $headers);
 
         $responseBody = curl_exec($curlHandler);
         $statusCode = curl_getinfo($curlHandler, CURLINFO_HTTP_CODE);
